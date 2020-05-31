2776044b2bf91e037fdcae03670a69319aef35f7
Dave Brace <dave.brace@gmail.com>
2020-05-31 10:01:03 -0500

    Improvements to arcana training

--- arcana.cmd
+++ arcana.cmd
@@ -3,26 +3,23 @@
 start:
   put get broad
   pause 0.5
-  put rem armband
-  pause 0.5
   goto use_armband
 
 use_armband:
-  gosub clear
   gosub exp_check
 
-  gosub cast_spell sw 10 24
-  gosub cast_spell suf 10 24
-  # gosub cast_spell substr 5 16
-  # gosub cast_spell tw 5 16
+  gosub cast_spell etf 15 5 5 fire
 
-  gosub cast_spell es 10 24
-  gosub cast_spell ignite 5 23 broad
+  gosub cast_spell sw 5 25 10
+  gosub cast_spell suf 5 25 10
+
+  gosub cast_spell es 1 25 8
+  gosub cast_spell ignite 5 15 8 broad
 
   goto use_armband
 
 mana_check:
-  if $mana <= 40 {
+  if $mana <= 30 {
     echo  *** Recovering mana ***
     pause 5
     goto mana_check
@@ -33,17 +30,28 @@ cast_spell:
   var spell_name $1
   var prep_mana $2
   var charge_mana $3
-  var spell_target $4
+  var harness_mana $4
+  var spell_target $5
 
   gosub mana_check
 
   put prep %spell_name %prep_mana
   pause 0.5
 
-  gosub charge_arm %charge_mana
+  gosub charge_arm 
+  gosub invoke_arm
+
+  gosub harness
 
   match cast You feel fully prepared to cast your spell
   matchwait 35
+  return
+
+harness:
+  matchre return You tap into
+  matchre harness ...wait
+  put harness %harness_mana
+  matchwait 2
   return
 
 exp_check:
@@ -51,21 +59,21 @@ exp_check:
   return
 
 charge_arm:
-  put charge armband $1
-  pause 0.5
-  gosub invoke_arm
+  matchre return You harness
+  matchre charge_arm ...wait
+  put charge armband %charge_mana
+  matchwait 6
   return
 
 invoke_arm:
-  matchre invoke_arm but fail|accidentally attune yourself
+  matchre invoke_arm but fail|accidentally attune yourself|...wait
   matchre return readying all of its mana|you only are able to attune yourself|manage to attune yourself exactly as you intended to|
-  pause 0.5
   put invoke my armband %charge_mana
   matchwait 30
   return
 
 cast:
-  put cast %spell_target
+  send cast %spell_target
   pause 0.5
   return
 
@@ -73,7 +81,6 @@ done:
   return
 
 move_out:
-  put wear armband
   put sheath broad in baldric
   pause 2
   put #parse ARCANA DONE
