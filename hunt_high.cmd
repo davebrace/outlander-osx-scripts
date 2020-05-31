d4f95e74e03a360d70b3fdbd787f6a4b3a991c1f
Dave Brace <dave.brace@gmail.com>
2020-05-31 10:01:17 -0500

    Latest hunt_high.cmd script changes

--- hunt_high.cmd
+++ hunt_high.cmd
@@ -1,38 +1,42 @@
 # debuglevel 5
+var check_exp YES
 
 start:
-  put stance set 65 67 50 100
+  put get pouch
+  pause 0.5
+  put wear pouch
   pause 0.5
 
-  gosub use_2he
-  gosub train_weap 
+  put stance set 65 65 53 100
+  pause 0.5
 
   gosub use_2hb
   gosub train_weap
 
-  gosub use_quarterstaff
+  gosub summon_quarter
+  gosub train_weap
+  gosub break_weapon
+
+  gosub summon_polearm
+  gosub train_weap
+  gosub break_weapon
+
+  gosub use_2he
+  gosub train_weap 
+
+  gosub use_broad
   gosub train_weap
 
-  gosub use_knuckles
-  gosub train_weap
-
-  gosub use_glaive
-  gosub train_weap
-
-  put stance set 100 0 82 100
+  put stance set 100 0 83 100
   pause 0.5
 
   gosub use_ht
   gosub train_thrown_weap
 
-  put stance set 65 67 50 100
-  pause 0.5
-
-  gosub use_broad
+  gosub use_brawling
   gosub train_weap
 
-  put stance set 100 0 82 100
-  pause 0.5
+  gosub train_magic
 
   goto end
 
@@ -47,10 +51,50 @@ use_ht:
   var skill HT
   return
 
-use_glaive:
-  var weapon glaive
-  var summon NO
-  var skill Polearm
+summon_quarter:
+  var skill staves
+  var weapon quarterstaff
+
+  put summon weapon %skill
+  pause 3
+
+  put shape %weapon to fire
+  pause 2
+
+  return
+
+summon_polearm:
+  var skill polearm
+  var weapon lance
+
+  put summon weapon %skill
+  pause 3
+
+  put shape %weapon to fire
+  pause 2
+
+  return
+
+break_weapon:
+  put break my %weapon
+  pause 2
+  return
+
+train_summoned:
+  var skill %1
+  var weapon %2
+
+  put summon weapon %skill
+  pause 3
+
+  put shape %weapon to electric
+  pause 3
+
+  gosub train_weap
+
+  put break %weapon
+  pause 0.5
+
   return
 
 use_broad:
@@ -65,20 +109,14 @@ use_2hb:
   var skill "2HB"
   return
 
-use_quarterstaff:
-  var weapon quarterstaff
-  var summon YES
-  var skill staves
-  return
-
 use_2he:
   var weapon sword
   var summon NO
   var skill "2HE"
   return
 
-use_knuckles:
-  var weapon knuckles
+use_brawling:
+  var weapon Brawling
   var summon NO
   var skill Brawling
   return
@@ -93,9 +131,15 @@ train_thrown_weap:
 
 train_weap:
   gosub swap_greaves
-  put .hunt %weapon %summon %skill
+  put .hunt %weapon 
   waitforre ^HUNT DONE
   echo *** %weapon MINDLOCKED ***
+  pause 0.5
+  return
+
+train_magic:
+  put .hunt_magic
+  waitforre ^HUNT DONE
   pause 0.5
   return
 
@@ -103,6 +147,6 @@ end:
   echo *** Weapons Mind Locked! ***
   put release cyclic
   pause 0.5
-  put #goto $destination
+  put #goto 199
   waitforre ^YOU HAVE ARRIVED
   put #flash
